be15035172ea2f1bd8a49d26cecd821fcc9fe79e
PaulFenton <paulfenton@dal.ca>
2020-08-22 15:00:38 -0400

    added dev/production environment to allow CORS for dev

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const cors = require('cors');
 var fs = require('fs');
 const app = express()
 const port = 8080  // 8080 required for Azure monitoring
 
 // read static .json file into memory for the lifetime of the app
 var states = JSON.parse(fs.readFileSync('./states.json', 'utf8'));
+
+// allow cross origin requests in development
+if (process.env.NODE_ENV === 'development') {
+    console.log("Setting development service config to allow CORS.")
+    app.use(cors());
+    app.options('*', cors());
+}
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
