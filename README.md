6cfe1a2e10a879400655cadabb9dd4e6066707a7
PaulFenton <paulfenton@dal.ca>
2020-08-22 20:43:15 -0400

    update to readme and cleanup

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 This is an example data service app based on Express & Node.js. This repo is a companion repository for a tutorial on my blog:
 [Basic Node.js Data-Service on Azure](https://fentonsoftware.com/basic-node-js-data-service-on-azure/)
+
+# To run the service
+
+## production
+```
+ng run start
+```
+## development
+```
+ng run dev
+```
+
+NOTE: If using Linux or Mac for development, run manually or undate the 'start' script in package.json to the following command:
+```
+NODE_ENV=production node index.js
+```
--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = 8080  // 8080 required for Azure monitoring
 // read static .json file into memory for the lifetime of the app
 var states = JSON.parse(fs.readFileSync('./states.json', 'utf8'));
 
-// allow cross origin requests in development
+// allow cross origin requests in development mode
 if (process.env.NODE_ENV === 'development') {
     console.log("Setting development service config to allow CORS.")
     app.use(cors());
     app.options('*', cors());
 }
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World for Census Data!'));
 
 //serve the states GeoJson
 app.get('/states', (req, res) => {
